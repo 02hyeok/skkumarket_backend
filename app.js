85aedcee7d14362443e02cf981b92c6c3c122693
JungHwan_Laptop <ljh4770@naver.com>
2024-11-26 15:23:56 +0900

    Edit: Successfully Connection to DB

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
+import pool from './config/db.js';
+
+dotenv.config();
 const app = express();
-const port = 3000
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const PORT = process.env.PORT || 3000;
 
 // const { application } = require('express');
 
@@ -13,7 +19,26 @@ app.use(bodyParser.json());
 
 app.use(cookieParser());
 
-
 app.get('/', (req, res) => res.send('Hello World! from SWE3002 Team11 SKKU market.'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.use(cors({
+  origin: 'http://localhost:3000',  // 필요한 프론트엔드 URL 추후에 연결 후  설정
+  credentials: true
+}));
+app.use(express.json());
+
+// MySQL 연결 테스트 API
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the API!');
+});
+
+app.get('/api/test', async (req, res) => {
+  const [rows] = await pool.query('SELECT 1 + 1 AS solution');
+  res.json({ solution: rows[0].solution });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- config/db.js
+++ config/db.js
@@ -6,11 +6,12 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 1000,
   queueLimit: 0,
 });
 
