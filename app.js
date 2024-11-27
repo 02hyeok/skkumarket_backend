e6005d002fe470a020dfcdbe15ec0f2d49b2cc6f
Lee Junseo <jsms4825@naver.com>
2024-11-27 19:52:45 +0900

    make dynamic routing

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 
 import pool from './config/db.js';
+import { readdir } from 'fs/promises';
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -38,6 +40,33 @@ app.get('/api/test', async (req, res) => {
   const [rows] = await pool.query('SELECT 1 + 1 AS solution');
   res.json({ solution: rows[0].solution });
 });
+
+
+/*
+  아래는 동적 라우팅입니다. (routes 폴더를 라우팅)
+  예 routes
+      ㄴ test
+        ㄴ a.js
+  URL: http://localhost:3000/test/a
+*/
+const addRoutes = async (basePath, routeBase = '') => {
+  const files = await readdir(basePath, { withFileTypes: true }); // 파일과 폴더 구분
+
+  for (const file of files) {
+      const fullPath = path.join(basePath, file.name);
+
+      if (file.isDirectory()) {     // 디렉토리가 감지되면, 재귀 호출 진행
+          await addRoutes(fullPath, `${routeBase}/${file.name}`);
+      } else if (file.isFile() && file.name.endsWith('.js')) {    // 파일이면 라우팅
+          const modulePath = new URL(`file://${fullPath}`);
+          const route = await import(modulePath.href);
+          const routeName = `${routeBase}/${file.name.replace('.js', '')}`;
+          app.use(routeName, route.default);
+      }
+  }
+};
+
+await addRoutes(path.resolve('./routes'));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
