db5c94415c6883ea070131ab57502a05b8f81c6a
Junhyeok Song <02hyeok@hdmedi.co.kr>
2024-11-29 10:54:14 +0900

    Revert "Merge branch 'master' into jhsong2"
    This reverts commit 0313b26a2743ce83537ea5ab555616b2e25e8177, reversing
    changes made to f5eed6d4e2bf09808fcf2982ee45b6b79d660f75.

--- controllers/utils.js
+++
@@ -1,8 +0,0 @@
-export const asyncHandler = fn => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-export const getCurrentTimestamp = () => {
-    const now = new Date();
-    return now.toISOString().slice(0, 19).replace('T', ' '); // 'YYYY-MM-DD HH:mm:ss' 형식
-};
