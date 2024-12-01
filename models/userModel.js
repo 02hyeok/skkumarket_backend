ca9601fe3aa7460690782fcb633d582eb7fab5f2
jsms4825 <jsms4825@naver.com>
2024-12-01 21:54:09 +0900

    modify login and register function

--- models/userModel.js
+++ models/userModel.js
@@ -9,15 +9,15 @@ userModel.getUserById = async (user_id) => {
     return rows[0]; // 결과가 없으면 undefined 반환
 };
 
-userModel.login = async (email, password) => {
-    const sql = 'SELECT id, student_id, nickname, major FROM User WHERE email = ? AND password = ?';
-    const [rows] = await pool.execute(sql, [email, password]);
+userModel.login = async (account_id, password) => {
+    const sql = 'SELECT id, student_id, nickname, major FROM User WHERE account_id = ? AND password = ?';
+    const [rows] = await pool.execute(sql, [account_id, password]);
     return rows[0]; // 결과가 없으면 undefined 반환
 };
 
-userModel.register = async (student_id, email, password, nickname, major) => {
-    const sql = 'INSERT INTO User (student_id, email, password, nickname, major) VALUES (?, ?, ?, ?, ?)';
-    const [result] = await pool.execute(sql, [student_id, email, password, nickname, major]);
+userModel.register = async (student_id, account_id, password, nickname, major) => {
+    const sql = 'INSERT INTO User (student_id, account_id, password, nickname, major) VALUES (?, ?, ?, ?, ?)';
+    const [result] = await pool.execute(sql, [student_id, account_id, password, nickname, major]);
     return result.affectedRows // 삽입된 행위 개수 반환 (성공: 1, 실패: 0)
 };
 
